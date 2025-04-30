a57bcb72e197bff5b81a830196e525eaa0070e2a
navuzz <easemyemail@gmail.com>
2025-04-30 15:38:18 +0530

    [+] INIT LANDING PAGE

---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,87 @@
+import { lazy } from 'react';
+
+// project-imports
+import ErrorBoundary from './ErrorBoundary';
+import Loadable from 'components/Loadable';
+import PagesLayout from 'layout/Pages';
+import SimpleLayout from 'layout/Simple';
+
+import { SimpleLayoutType } from 'config';
+
+const MaintenanceError = Loadable(lazy(() => import('pages/maintenance/error/404')));
+const MaintenanceError500 = Loadable(lazy(() => import('pages/maintenance/error/500')));
+const MaintenanceUnderConstruction = Loadable(lazy(() => import('pages/maintenance/under-construction/under-construction')));
+const MaintenanceUnderConstruction2 = Loadable(lazy(() => import('pages/maintenance/under-construction/under-construction2')));
+const MaintenanceComingSoon = Loadable(lazy(() => import('pages/maintenance/coming-soon/coming-soon')));
+const MaintenanceComingSoon2 = Loadable(lazy(() => import('pages/maintenance/coming-soon/coming-soon2')));
+
+// render - sample page
+const SamplePage = Loadable(lazy(() => import('pages/extra-pages/sample-page')));
+const Landing = Loadable(lazy(() => import('pages/landing')));
+const ContactUS = Loadable(lazy(() => import('pages/contact-us')));
+const PricingPage = Loadable(lazy(() => import('pages/extra-pages/price/price1')));
+const PricingPage2 = Loadable(lazy(() => import('pages/extra-pages/price/price2')));
+
+// ==============================|| MAIN ROUTES ||============================== //
+
+const MainRoutes = {
+  path: '/',
+  children: [
+    {
+      path: '/',
+      element: <SimpleLayout layout={SimpleLayoutType.LANDING} />,
+      children: [
+        {
+          path: 'landing',
+          element: <Landing />
+        }
+      ]
+    },
+    {
+      path: '/',
+      element: <SimpleLayout layout={SimpleLayoutType.SIMPLE} />,
+      children: [
+        {
+          path: 'contact-us',
+          element: <ContactUS />
+        }
+      ]
+    },
+    {
+      path: '/maintenance',
+      element: <PagesLayout />,
+      children: [
+        {
+          path: '404',
+          element: <MaintenanceError />
+        },
+        {
+          path: '500',
+          element: <MaintenanceError500 />
+        },
+        {
+          path: 'under-construction',
+          element: <MaintenanceUnderConstruction />
+        },
+        {
+          path: 'under-construction2',
+          element: <MaintenanceUnderConstruction2 />
+        },
+        {
+          path: 'coming-soon',
+          element: <MaintenanceComingSoon />
+        },
+        {
+          path: 'coming-soon2',
+          element: <MaintenanceComingSoon2 />
+        }
+      ]
+    },
+    {
+      path: '*',
+      element: <MaintenanceError />
+    }
+  ]
+};
+
+export default MainRoutes;
